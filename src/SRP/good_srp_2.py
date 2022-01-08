41e3b6d5b29a04ee8f769724c151edc6531f9d05
gonczor <wiktor.gonczaronek@gmail.com>
2022-01-08 10:57:16 +0100

    Second example of SRP

---
+++ src/SRP/good_srp_2.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+
+from database import cars_db
+
+
+class CarModel:
+    id: int
+    make: str
+    model: str
+    last_service: int
+
+    def __init__(self, id, make, model, last_service):
+        self.id = id
+        self.make = make
+        self.model = model
+        self.last_service = last_service
+
+    @classmethod
+    def get_by_id(cls, id: int) -> "CarManager":
+        data = cars_db[id]
+        return cls(
+            id=id,
+            **data
+        )
+
+    @property
+    def requires_service(self) -> bool:
+        return datetime.today().year - 1 > self.last_service
+
+    def __repr__(self):
+        return f"{self.id}: {self.make} {self.model}"
+
+
+class CarRepository:
+    @staticmethod
+    def get_by_id(id: int) -> CarModel:
+        data = cars_db[id]
+        return CarModel(
+            id=id,
+            **data
+        )
+
+
+if __name__ == "__main__":
+    car = CarRepository.get_by_id(1)
+    print(f"{car} | requires service: {car.requires_service}")
+    car = CarRepository.get_by_id(2)
+    print(f"{car} | requires service: {car.requires_service}")
